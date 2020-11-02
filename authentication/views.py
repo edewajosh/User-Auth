095860e7f57cd9ddb0d86aaa91da491db855eff4
edewajosh <joshedewa@gmail.com>
2020-11-02 21:40:27 +0300

    Created and user management endpoint

--- authentication/views.py
+++ authentication/views.py
@@ -1,3 +1,8 @@
 from django.shortcuts import render
+from authentication.serializers import UserSerializer
+from rest_framework.viewsets import ModelViewSet
+from authentication.models import User
 
-# Create your views here.
+class UserViewSet(ModelViewSet):
+    queryset = User.objects.all()
+    serializer_class = UserSerializer
