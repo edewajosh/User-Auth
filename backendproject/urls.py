656e02c71cc01e87a0f3d2758281714b9bed89fc
edewajosh <joshedewa@gmail.com>
2020-11-04 19:28:41 +0300

    - Added Simple JWT token for authentication

--- backendproject/urls.py
+++ backendproject/urls.py
@@ -1,8 +1,16 @@
 from django.contrib import admin
 from django.urls import path, include
+from rest_framework_simplejwt.views import (
+    TokenObtainPairView,
+    TokenRefreshView,
+)
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('api/', include('authentication.urls'))
+    path('api/', include('authentication.urls')),
+
+    # Obtain and refresh authentication tokens
+    path('api/token/', TokenObtainPairView.as_view(), name='token_obtain_pair'),
+    path('api/token/refresh/', TokenRefreshView.as_view(), name='token_refresh'),
 
 ]
