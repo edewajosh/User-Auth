79a88583ed492ef4bf1dbad4ac8cae43090e263b
edewajosh <joshedewa@gmail.com>
2020-11-04 20:21:54 +0300

    Added permission restriction to the users' endpoint

--- authentication/views.py
+++ authentication/views.py
@@ -1,8 +1,12 @@
 from django.shortcuts import render
-from authentication.serializers import UserSerializer
 from rest_framework.viewsets import ModelViewSet
+from rest_framework.permissions import IsAuthenticatedOrReadOnly
+
+from authentication.serializers import UserSerializer
+
 from authentication.models import User
 
 class UserViewSet(ModelViewSet):
+    permission_classes = [IsAuthenticatedOrReadOnly]
     queryset = User.objects.all()
     serializer_class = UserSerializer
