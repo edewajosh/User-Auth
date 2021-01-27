b59a884a302b29a261f1b058e003f849111dd59d
edewajosh <joshedewa@gmail.com>
2021-01-27 16:34:30 +0300

    Fixed the partial update bug

--- authentication/serializers.py
+++ authentication/serializers.py
@@ -22,14 +22,7 @@ class UserSerializer(ModelSerializer):
         user.save()
         return user
 
-    def update(self, instance, validated_data):
-        password = validated_data.pop('password')
-        user = super(UserSerializer, self).update(instance, validated_data)
-        if password:
-            user.set_password(password)
-            user.save()
-            return user
-
+    
     class Meta:
         model = User
         fields = ['first_name', 'last_name', 'email', 'password','confirm_password', 'is_admin', 'is_active', 'is_staff']
--- authentication/views.py
+++ authentication/views.py
@@ -17,12 +17,23 @@ class UserViewSet(ModelViewSet):
         return Response('User has been deleted successfully')
 
     def partial_update(self, request, *args, **kwargs):
-        print(request.data, "patch")
-        serialized = UserSerializer(request.user, data=request.data, partial=True)
-        return Response(status=status.HTTP_202_ACCEPTED)
+        kwargs['partial'] = True
+        return self.update(request, *args, **kwargs)
 
-    def perform_create(self, serializer):
-        serializer.save()
+    def update(self, request, *args, **kwargs):
+        partial = kwargs.pop('partial', True)
+        instance = self.get_object()
+        serializer = self.get_serializer(instance, data=request.data, partial=partial)
+        serializer.is_valid(raise_exception=True)
+        self.perform_create(serializer)
+
+        if getattr(instance, '_prefetched_objects_cache', None):
+            # if 'prefetch_related' has been applied to a queryset, we need to
+            # forcibly invalidate the prefetch cache on the instance
+            instance._prefetched_objects_cache = {}
+        
+        return Response(serializer.data)
+
 
     def get_permissions(self):
         """
